d57897a0b7e09650bf30ac4d0306351924072ce1
NaiveTomcat <tomdang@naivetomcat.cn>
2021-07-22 10:41:26 +0800

    modified a lot things

--- src/sitegraph.cpp
+++ src/sitegraph.cpp
@@ -1,8 +1,14 @@
 #include "sitegraph.hpp"
+#include <cmath>
 #include <iostream>
 
+inline int64_t shrink(int length, int sim_range) {
+    return length / (sim_range + 1);
+}
+
 std::vector<std::tuple<Site *, int64_t, std::vector<std::string>>>
-Contig::get_first_site(int overlap_length, int depth, int length)
+Contig::get_first_site(int overlap_length, Contig *from, int simplify_range,
+                       int depth, int length)
 {
     if (sites.size() != 0) {
         // std::cout << "Find" << std::endl; // DEBUG
@@ -11,9 +17,24 @@ Contig::get_first_site(int overlap_length, int depth, int length)
             auto site = sites[i];
             while (site->pos < overlap_length)
                 site = sites[++i];
+            auto total_length = site->pos + length;
+            if (simplify_range >= 0) {
+                // for (auto path : from->sites.back()->edges) {
+                //     if ((path->to->id == site->id) &&
+                //         (std::abs(path->len - total_length) <=
+                //         simplify_range)) return {};
+                // }
+                auto pr = from->sites.back()->edgelengths.equal_range(site->id);
+                while (pr.first != pr.second) {
+                    if ((std::abs(pr.first->second - total_length) <=
+                         simplify_range))
+                        return {};
+                    pr.first++;
+                }
+            }
             return std::vector<
                 std::tuple<Site *, int64_t, std::vector<std::string>>>{
-                {site, site->pos, {name}}};
+                {site, total_length, {name}}};
         }
     }
     // if (had_memo[occurance]) // Memorandum
@@ -46,22 +67,49 @@ Contig::get_first_site(int overlap_length, int depth, int length)
     // DFS
     // std::cout << "DFS at depth " << depth << std::endl; // DEBUG
     std::vector<std::tuple<Site *, int64_t, std::vector<std::string>>>
-        next_sites;
+                                              next_sites;
+    std::unordered_multimap<Site*, int64_t> lengths;
     for (auto contig : next) {
         if (contig == nullptr) {
             // std::cout << "Nullptr" << std::endl; // DEBUG
             continue;
         }
-        auto temp_sites = contig->get_first_site(overlap_length, depth + 1,
-                                                 length + sequence.length());
+        auto temp_sites =
+            contig->get_first_site(overlap_length, from, simplify_range,
+                                   depth + 1, length + sequence.length());
         for (auto temp_site_iter = temp_sites.begin();
              temp_site_iter < temp_sites.end(); temp_site_iter++) {
-            std::get<1>(*temp_site_iter) += sequence.length();
+            // std::get<1>(*temp_site_iter) += sequence.length();
             std::get<2>(*temp_site_iter).push_back(name);
         }
-        next_sites.insert(next_sites.end(),
-                          std::make_move_iterator(temp_sites.begin()),
-                          std::make_move_iterator(temp_sites.end()));
+        if (simplify_range < 0)
+            next_sites.insert(next_sites.end(),
+                              std::make_move_iterator(temp_sites.begin()),
+                              std::make_move_iterator(temp_sites.end()));
+        else
+            std::copy_if(
+                std::make_move_iterator(temp_sites.begin()),
+                std::make_move_iterator(temp_sites.end()),
+                std::back_inserter(next_sites),
+                [&simplify_range, &lengths](
+                    const std::tuple<Site *, int64_t, std::vector<std::string>>
+                        &v) {
+                    // for (auto l : lengths)
+                    //     if ((l.first == std::get<0>(v)->id) &&
+                    //         (std::abs(l.second - std::get<1>(v)) <=
+                    //          simplify_range))
+                    //         return false;
+                    // lengths.push_back({std::get<0>(v)->id, std::get<1>(v)});
+                    auto pr = lengths.equal_range(std::get<0>(v));
+                    while (pr.first != pr.second) {
+                        if (std::abs(pr.first->second - std::get<1>(v)) <=
+                            simplify_range)
+                            return false;
+                        pr.first++;
+                    }
+                    lengths.insert({std::get<0>(v), std::get<1>(v)});
+                    return true;
+                });
     }
     // reachable_site_memo[occurance] = next_sites;
     // had_memo[occurance]            = true;
